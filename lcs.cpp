bf007a06361afde45f81392dbe9bd4fc0c169b03
altruistcoder <rishabhaggarwal0101@gmail.com>
2019-08-19 14:21:41 +0530

    Longest Common Subsequence

--- lcs.cpp
+++ lcs.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <string.h>
-
 using namespace std;
 
 int lcs(char x[],char y[])
 {
     int i, j, m=strlen(x), n=strlen(y), r[m+1][n+1];
     for(i=0; i<=m; i++)
-    {
         for(j=0; j<=n; j++)
         {
             if (i==0 || j==0)
@@ -17,7 +15,6 @@ int lcs(char x[],char y[])
             else
                 r[i][j] = max(r[i-1][j], r[i][j-1]);
         }
-    }
     return r[m][n];
 }
 
@@ -26,6 +23,6 @@ int main()
     char x[50], y[50];
     cout<<"Enter the two strings: ";
     cin>>x>>y;
-    cout<<"The length of the longest common subsequence is: "<<lcs(x, y);
+    cout<<"The length of the longest common subsequence is: "<<lcs(x, y)<<endl;
     return 0;
 }
